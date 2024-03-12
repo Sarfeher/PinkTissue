a7fb55c6003b1aee7c41b9320a662d0bab2a10cf
Mark Veszelka <120094709+markveszelka@users.noreply.github.com>
2024-03-12 13:24:59 +0100

    Add wait to base class

--- src/main/java/org/example/testCases/BaseTestCase.java
+++ src/main/java/org/example/testCases/BaseTestCase.java
@@ -2,12 +2,17 @@ package org.example.testCases;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public abstract class BaseTestCase {
     protected final WebDriver webDriver;
+    protected final WebDriverWait wait;
 
     public BaseTestCase(WebDriver webDriver) {
         this.webDriver = webDriver;
+        this.wait = new WebDriverWait(webDriver, Duration.ofSeconds(20));
         PageFactory.initElements(webDriver, this);
     }
 }
