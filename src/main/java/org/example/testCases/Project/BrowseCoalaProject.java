690e7a5308961d0d8b2d977c95ea0c7b39114045
Sarfeher <132908720+Sarfeher@users.noreply.github.com>
2024-03-12 19:43:02 +0100

    browseCoalaProject params add

--- src/main/java/org/example/testCases/Project/BrowseCoalaProject.java
+++ src/main/java/org/example/testCases/Project/BrowseCoalaProject.java
@@ -5,32 +5,24 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
 public class BrowseCoalaProject extends BaseTestCase{
+    @FindBy(id = "project-filter-text") private WebElement searchBar;
+    @FindBy(xpath = "//*[@id=\"project-name-val\"]") private WebElement project;
+    @FindBy(xpath = "//*[@id='projects']/div/table/tbody/tr/td[1]/a") private WebElement projectName;
 
     public BrowseCoalaProject(WebDriver webDriver) {
         super(webDriver);
     }
 
     public void run(String projectName) {
-        WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(1));
-
-        WebElement searchBar = webDriver.findElement(By.xpath("//*[@id='project-filter-text']"));
+        WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
         searchBar.sendKeys(projectName);
-        searchBar.submit();
-
-        webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
-
-        WebElement nameOfProject = webDriver.findElement(By.xpath("//*[@id=\"projects\"]/div/table/tbody/tr/td[1]/a"));
-        nameOfProject.click();
-
-
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"project-name-val\"]")));
-
-
+        wait.until(ExpectedConditions.visibilityOf(project));
     }
 }
