d400b8016bd1580a3e48204a907dfae2b6836a18
Mark Veszelka <120094709+markveszelka@users.noreply.github.com>
2024-03-12 09:23:48 +0100

    Add PageFactory initElements to BaseTestCase

--- src/main/java/org/example/testCases/BaseTestCase.java
+++ src/main/java/org/example/testCases/BaseTestCase.java
@@ -1,11 +1,13 @@
 package org.example.testCases;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
 
 public abstract class BaseTestCase {
     protected final WebDriver webDriver;
 
     public BaseTestCase(WebDriver webDriver) {
         this.webDriver = webDriver;
+        PageFactory.initElements(webDriver, this);
     }
 }
